ef79564b855a820f677cf8ae4852139d7ff934e0
Sebastian Weigand <sab@sab.systems>
2018-07-18 14:05:47 -0400

    Preflight Script Update
    * Updated welcome message to be more friendly
    * Created functions to execute command checking and config checking
    * Added check for Terraform binary
    * Added warning about existing .terraform directory (see below)
    * Reworked gcloud variable state checking (see below)
    * Updated Terraform bucket name to include "cloudlamp"
    * Preflight now calls `terraform init`
    
    Existing Terraform Directory
    ----------------------------
    If an existing .terraform directory is present, there is a chance
    TF will attempt to reconcile state with a nonexistent project (or
    components therein). Discarding the local file alleviates the need
    to check these parameters. Useful in debugging.
    
    Gcloud Variable Checking
    ------------------------
    Upon testing in Cloud Shell, it turns out it emits a message which
    begins with "Your active configuration", which was not present on
    other CLI clients. Now we use wildcard matching to ensure the right
    variables are set prior to execution.

--- nfs/preflight.sh
+++ nfs/preflight.sh
@@ -4,25 +4,63 @@
 # Sebastian Weigand <tdg@google.com>
 # Fernando Sanchez <fersanchez@google.com>
 
-echo "Welcome to CloudLAMP!"
+echo "
+  _______             ____   ___   __  ______
+ / ___/ /__  __ _____/ / /  / _ | /  |/  / _ \\
+/ /__/ / _ \/ // / _  / /__/ __ |/ /|_/ / ___/
+\___/_/\___/\_,_/\_,_/____/_/ |_/_/  /_/_/
+"
 
+# =============================================================================
+# Functions
+# =============================================================================
+
+# Prefixes output and writes to STDERR:
 error() {
   echo -e "\n\nCloudLAMP Error: $@\n" >&2
 }
 
-printf '%-50s' "Checking for gcloud..."
-command -v gcloud >/dev/null 2>&1 || {
-  error "The 'gcloud' command was not found. Please install the Google Cloud SDK from: https://cloud.google.com/sdk/downloads"
-  exit 1
-}
-echo "[ OK ]"
+# Checks for command presence in $PATH, errors:
+check_command() {
+  TESTCOMMAND=$1
+  HELPTEXT=$2
 
-printf '%-50s' "Checking for kubectl..."
-command -v kubectl >/dev/null 2>&1 || {
-  error "The 'kubectl' command was not found. You can install it via: 'gcloud components install kubectl'"
-  exit 1
+  printf '%-50s' "Checking for $TESTCOMMAND..."
+  command -v $TESTCOMMAND >/dev/null 2>&1 || {
+    error "The '$TESTCOMMAND' command was not found. $HELPTEXT"
+    
+    exit 1
+  }
+  echo "[ OK ]"
 }
-echo "[ OK ]"
+
+# Tests variables for valid values:
+check_config() {
+  PARAM=$1
+  VAR=$2
+  if [[ $PARAM = *"(unset)"* ]]; then
+    error "Please set the gcloud variable '$VAR' via:
+    gcloud config set $VAR <value>" 
+   
+    exit 1
+  fi
+}
+
+# =============================================================================
+# Sanity checking
+# =============================================================================
+
+# Notify on existence of potentially conflicting Terraform directory, in cases
+# where this script was executed yet the state has drifted:
+if [ -d .terraform ]; then
+  echo -e "\nWarning: An existing '.terraform/' state directory was found. If this
+  causes issues, remove it and re-run this script.\n"
+fi
+
+# Check for our requisite binaries:
+check_command gcloud "Please install the Google Cloud SDK from: https://cloud.google.com/sdk/downloads"
+check_command kubectl "You can install it via: 'gcloud components install kubectl'"
+check_command terraform "Visit https://www.terraform.io/downloads.html for more information."
 
 # This executes all the gcloud commands in parallel and then assigns them to separate variables:
 # Needed for non-array capabale bashes, and for speed.
@@ -33,15 +71,10 @@ PARAMS=$(cat <(gcloud config get-value compute/zone 2>&1) \
   <(gcloud auth application-default print-access-token 2>&1))
 read GCP_ZONE GCP_REGION GCP_PROJECT GCP_AUTHTOKEN <<< $(echo $PARAMS)
 
-for VAR in $GCP_PROJECT $GCP_REGION $GCP_ZONE; do
-	if [[ $VAR == "(unset)" ]]; then
-    error "Please set the 'compute/region', 'compute/zone', and 'project' in your gcloud config:
-    gcloud config set <component> <value>"
-    echo "Your current gcloud config:"
-    gcloud config list
-		exit 1
-	fi
-done
+# Check for our requisiste gcloud parameters:
+check_config $GCP_PROJECT "project"
+check_config $GCP_REGION "compute/region"
+check_config $GCP_ZONE "compute/zone"
 
 if [[ $GCP_AUTHTOKEN == *"ERROR"* ]]; then
   error "You do not have application-default credentials set, please run this command:
@@ -50,9 +83,15 @@ if [[ $GCP_AUTHTOKEN == *"ERROR"* ]]; then
 fi
 echo "[ OK ]"
 
+# =============================================================================
+# Initialization
+# =============================================================================
+
+TERRAFORM_BUCKET=$GCP_PROJECT-cloudlamp-terraform
+
 # Create a GCS bucket for Terraform state; ignore if it's already there:
-printf '%-50s' "Checking Terraform state bucket..."
-BUCKET_STATUS=$(gsutil mb -l $GCP_REGION -c Regional gs://$GCP_PROJECT-terraform-state 2>&1)
+printf '%-50s' "Checking/creating Terraform state bucket..."
+BUCKET_STATUS=$(gsutil mb -l $GCP_REGION -c Regional gs://$TERRAFORM_BUCKET 2>&1)
 if [[ $? != 0 ]]; then
   if [[ $BUCKET_STATUS != *"409"* ]]; then
     error "Error creating Terraform state bucket:\n\n$BUCKET_STATUS"
@@ -67,7 +106,7 @@ cat << EOF > backend.tf
 # Note: This file is generated from preflight.sh:
 terraform {
  backend "gcs" {
-   bucket  = "$GCP_PROJECT-terraform-state"
+   bucket  = "$TERRAFORM_BUCKET"
    prefix  = "/tf/terraform.tfstate"
    project = "$GCP_PROJECT"
  }
@@ -76,7 +115,7 @@ EOF
 echo "[ OK ]"
 
 # Populate Terraform default variables:
-printf '%-50s' "Setting Terraform variables..."
+printf '%-50s' "Creating Terraform variables file..."
 cat << EOF > terraform.tfvars
 # Note: This file is generated from preflight.sh:
 gcp_region  = "$GCP_REGION"
@@ -87,6 +126,8 @@ echo "[ OK ]"
 
 echo "
 Success! You are now ready to deploy CloudLAMP via Terraform via:
-  terraform init
   terraform [ plan | apply ]
 "
+
+# Initialize Terraform with the 3 requisite providers:
+terraform init
